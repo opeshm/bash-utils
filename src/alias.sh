bffaa291deb8cce4798287beff7fbcac45a3bf78
Mario Dominguez <opes_hm@yahoo.es>
2015-04-12 19:04:24 +0200

    Added functions.

--- src/alias.sh
+++ src/alias.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-## change directory
+# =============================
+# CHANGE DIRECTORY
+# =============================
 alias cd..='cd ..'
 alias cd...='cd ../..'
 
 ## lla 
 alias lla='ls -la --color=auto'
 alias bashutils='echo BashUtils v${BASHUTILS_VERSION}'
+
+
--- src/bash-utils.sh
+++ src/bash-utils.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
 
+# =============================
 # EXPORT VERSION VARIABLE
-export BASHUTILS_VERSION='0.2'
+# =============================
+export BASHUTILS_VERSION='0.3'
 
+# =============================
 # EXPORT PATH
+# =============================
 PATHSCRIPTS=`echo ${PATH} | grep -i bash-utils/scripts`
 if [ "${PATHSCRIPTS}" == "" ]; then
     export PATH=${PATH}:~/.bash-utils/scripts
 fi
 
+# =============================
 # ADDING ALIAS
+# =============================
 if [ -f ~/.bash-utils/alias.sh ]; then
     . ~/.bash-utils/alias.sh
+fi
+
+# =============================
+# ADDING FUNCTIONS
+# =============================
+if [ -f ~/.bash-utils/functions.sh ]; then
+    . ~/.bash-utils/functions.sh
 fi
 
