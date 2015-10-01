613aaadc33912f394c94565ca02f24d8f82f5b7c
Mario Domínguez <opes_hm@yahoo.es>
2015-10-01 13:40:10 +0200

    Add completion to bash

--- src/bash-utils.sh
+++ src/bash-utils.sh
@@ -6,6 +6,17 @@
 export BASHUTILS_VERSION='0.3'
 
 # =============================
+# ADDING COMPLETION
+# =============================
+if ! shopt -oq posix; then
+  if [ -f /usr/share/bash-completion/bash_completion ]; then
+    . /usr/share/bash-completion/bash_completion
+  elif [ -f /etc/bash_completion ]; then
+    . /etc/bash_completion
+  fi
+fi
+
+# =============================
 # EXPORT PATH
 # =============================
 PATHSCRIPTS=`echo ${PATH} | grep -i bash-utils/scripts`
@@ -26,4 +37,6 @@ fi
 if [ -f ~/.bash-utils/functions.sh ]; then
     . ~/.bash-utils/functions.sh
 fi
+
+
 
