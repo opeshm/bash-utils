98b0c656eb1f973655924a81f55a36a4a1df8306
Mario Dominguez <opes_hm@yahoo.es>
2016-08-04 14:48:42 +0200

    Fix tput error on start session

--- src/functions.sh
+++ src/functions.sh
@@ -3,15 +3,16 @@
 # =============================
 # VARIABLES
 # =============================
-export bold=$(tput bold)
-export underline=$(tput sgr 0 1)
-export reset=$(tput sgr0)
-export purple=$(tput setaf 171)
-export red=$(tput setaf 1)
-export green=$(tput setaf 76)
-export tan=$(tput setaf 3)
-export blue=$(tput setaf 38)
-
+if [[ $- == *i* ]]; then
+    export bold=$(tput bold)
+    export underline=$(tput sgr 0 1)
+    export reset=$(tput sgr0)
+    export purple=$(tput setaf 171)
+    export red=$(tput setaf 1)
+    export green=$(tput setaf 76)
+    export tan=$(tput setaf 3)
+    export blue=$(tput setaf 38)
+fi
 
 # =============================
 # FUNCTIONS
