a93631b6b94fa89c5ad10074d86fc30cd05392b7
Mario Dominguez <mario.dominguez@infojobs.net>
2016-12-20 11:26:48 +0100

    Added script to show git log in single line
    Added new script to initialize docker machine with default environment
    
    New alias to set java version globaly

--- src/alias.sh
+++ src/alias.sh
@@ -9,8 +9,9 @@ alias ls='ls --color=auto'
 alias ll='ls -l'
 alias la='ls -la'
 
-## lla 
+## lla
 alias lla='ls -la --color=auto'
 alias bashutils='echo BashUtils v${BASHUTILS_VERSION}'
 
-
+alias j18="jenv global 1.8 && java -version"
+alias j17="jenv global 1.7 && java -version"
