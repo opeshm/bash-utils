fc7eda1136b9158bc455b1fb306cceb0d74ac98e
Mario Dominguez <opes_hm@yahoo.es>
2018-11-28 09:20:31 +0100

    New atom plugin list

--- functions/atom-fn.sh
+++ functions/atom-fn.sh
@@ -6,28 +6,20 @@ function atom-full-install () {
 }
 
 function atom-install() {
-  ATOM_DEB_URL="https://atom-installer.github.com/v1.25.0/atom-amd64.deb?s=1521055093&amp;ext=.deb"
-  ATOM_DEB_PATH="${HOME}/tmp/atom-amd64.deb"
-
-  mkdir -p ${HOME}/tmp
-  wget -O ${ATOM_DEB_PATH} $ATOM_DEB_URL
-
-  if [ -f ${ATOM_DEB_PATH} ]; then
-    sudo apt install ${ATOM_DEB_PATH}
-    rm ${ATOM_DEB_PATH}
-  else
-    echo "No se pudo descargar el paquete"
-  fi
+  snap install atom --classic
 }
 
 function atom-install-packages() {
-  apm install atom-ide-ui
-  apm install city-lights-icons
   apm install docker
+  apm install ide-bash
+  apm install ide-docker
+  apm install ide-json
+  apm install ide-php
   apm install language-docker
-  apm install language-terra
   apm install language-terraform
   apm install minimap
   apm install minimap-autohider
-  apm install platformio-ide-terminal
+  apm install nuclide
+  apm install project-manager
+  apm install teletype
 }
